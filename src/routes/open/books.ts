aa9a24146a2ae1043d137cb3f174e42e0b1568f2
Mathew Miller <mathew.evan85@gmail.com>
2025-04-27 17:11:45 -0700

    Adds the books router and the first get command for usage in postman.

---
+++ src/routes/open/books.ts
@@ -0,0 +1,74 @@
+//Access the connection to Postgres Database
+import { pool, validationFunctions } from '../../core/utilities';
+import express, { Request, Response, Router } from 'express';
+
+const booksRouter: Router = express.Router();
+
+const isStringProvided = validationFunctions.isStringProvided;
+const isNumberProvided = validationFunctions.isNumberProvided;
+//
+// const format = (resultRow) =>
+//     `{${resultRow.priority}} - [${resultRow.name}] says: ${resultRow.message}`;
+//
+// const formatKeep = (resultRow) => ({
+//     ...resultRow,
+//     formatted: `{${resultRow.priority}} - [${resultRow.name}] says: ${resultRow.message}`,
+// });
+//
+// function mwValidAuthorQuery(
+//     request: Request,
+//     response: Response,
+//     next: NextFunction
+// ) {
+//     const priority: string = request.query.priority as string;
+//     if (
+//         validationFunctions.isStringProvided(priority) &&
+//         parseInt(priority) >= 1 &&
+//         parseInt(priority) <= 3
+//     ) {
+//         next();
+//     } else {
+//         console.error('Author name does not exist or invalid input type');
+//         response.status(400).send({
+//             message:
+//                 'Author name does not exist or invalid input type',
+//         });
+//     }
+// }
+
+booksRouter.get('/isbn/:isbn13', (request: Request, response: Response) => {
+    const theQuery =
+        'SELECT * FROM BOOKS WHERE isbn13 = $1';
+    const values = [request.params.isbn13];
+
+    pool.query(theQuery, values)
+        .then((result) => {
+            if (result.rowCount > 0) {
+                response.send({
+                    entries: result.rows
+                });
+            } else {
+                response.status(404).send({
+                    message: 'ISBN13 not found',
+                });
+            }
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET by ISBN13');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
+});
+
+booksRouter.get('/', (request: Request, response: Response) => {
+    response.send('<h1>Hello Books!</h1>');
+});
+
+booksRouter.post('/', (request: Request, response: Response) => {
+    response.send('<h1>Hello Books!</h1>');
+});
+
+export { booksRouter };
