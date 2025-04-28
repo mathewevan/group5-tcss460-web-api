fcdd2885456a3d709b0d0479dd396a71aa0ff9bb
Ken Egawa <kenegawa03@gmail.com>
2025-04-27 22:45:00 -0700

    api doc updated + further changes in desc
    Renamed old books.ts and changed route name to prevent misspellings.

--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
 
 import { messageRouter } from './message';
-import { booksRouter } from './books';
+import { booksRouter } from './books_old';
 
 const openRoutes: Router = express.Router();
 
 openRoutes.use('/message', messageRouter);
-openRoutes.use('/books', booksRouter)
+openRoutes.use('/books_old', booksRouter)
 
 export { openRoutes };
